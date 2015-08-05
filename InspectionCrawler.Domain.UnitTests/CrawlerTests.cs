3a5c146c45b3b8f0e49a469209931f2e2edb7a6f
Daniel Blankensteiner <dab@pentia.dk>
2015-08-05 21:52:07 +0200

    Adding more tests and updating nuget package

--- InspectionCrawler.Domain.UnitTests/CrawlerTests.cs
+++ InspectionCrawler.Domain.UnitTests/CrawlerTests.cs
@@ -10,16 +10,24 @@ namespace InspectionCrawler.Domain.UnitTests
 {
     public class CrawlerTests
     {
+        private readonly IFixture _fixture;
+        private readonly Uri _uri;
+
+        public CrawlerTests()
+        {
+            _fixture = new Fixture().Customize(new AutoMoqCustomization());
+            _uri = new Uri("http://www.google.com/");
+        }
+
         [Fact]
         public void CallFlushOnce()
         {
             // Arrange
-            var fixture = new Fixture().Customize(new AutoMoqCustomization());
-            var logHandler = fixture.Freeze<Mock<ILogHandler>>();
-            var crawler = fixture.Create<Crawler>();
+            var logHandler = _fixture.Freeze<Mock<ILogHandler>>();
+            var crawler = _fixture.Create<Crawler>();
 
             // Act
-            crawler.Crawl(new Uri("http://www.google.com/"));
+            crawler.Crawl(_uri);
 
             // Assert
             logHandler.Verify(x => x.Flush(), Times.Once);
@@ -29,16 +37,82 @@ namespace InspectionCrawler.Domain.UnitTests
         public void CallCrawlOnce()
         {
             // Arrange
-            var fixture = new Fixture().Customize(new AutoMoqCustomization());
-            var crawl = fixture.Freeze<Mock<ICrawl>>();
-            var crawler = fixture.Create<Crawler>();
-            var uri = new Uri("http://www.google.com/");
+            var crawl = _fixture.Freeze<Mock<ICrawl>>();
+            var crawler = _fixture.Create<Crawler>();
 
             // Act
-            crawler.Crawl(uri);
+            crawler.Crawl(_uri);
 
             // Assert
-            crawl.Verify(x => x.Crawl(uri, It.IsAny<Action<Page>>()), Times.Once);
+            crawl.Verify(x => x.Crawl(_uri, It.IsAny<Action<Page>>()), Times.Once);
+        }
+
+        [Fact]
+        public void CallCrawlStartingOnInspectorsOnce()
+        {
+            // Arrange
+            var inspector = new Mock<IInspector>();
+            var crawler = new Crawler(new Mock<ICrawl>().Object, new Mock<ILogHandler>().Object, new Mock<IExaminerFactory>().Object, new [] { inspector.Object });
+
+            // Act
+            crawler.Crawl(_uri);
+
+            // Assert
+            inspector.Verify(x => x.CrawlStarting(), Times.Once);
+        }
+
+        [Fact]
+        public void CallCrawlCompletedOnInspectorsOnce()
+        {
+            // Arrange
+            var inspector = new Mock<IInspector>();
+            var crawler = new Crawler(new Mock<ICrawl>().Object, new Mock<ILogHandler>().Object, new Mock<IExaminerFactory>().Object, new[] { inspector.Object });
+
+            // Act
+            crawler.Crawl(_uri);
+
+            // Assert
+            inspector.Verify(x => x.CrawlCompleted(), Times.Once);
+        }
+
+        [Fact]
+        public void LogWhenInspectorsThrowExceptionAtCrawlStarting()
+        {
+            // Arrange
+            var inspector = new Mock<IInspector>();
+            var exception = new Exception("Test exception");
+            inspector.Setup(i => i.CrawlStarting()).Throws(exception);
+            var logHandler = new Mock<ILogHandler>();
+            var crawler = new Crawler(new Mock<ICrawl>().Object, logHandler.Object, new Mock<IExaminerFactory>().Object, new[] { inspector.Object });
+
+            // Act
+            crawler.Crawl(_uri);
+
+            // Assert
+            logHandler.Verify(x => x.Log(It.Is<InspectorLogMessage>(
+                l => l.Inspector.Name == inspector.Object.Name && 
+                l.LogMessage.LogType == LogType.Error && 
+                l.LogMessage.Exception == exception)), Times.Once);
+        }
+
+        [Fact]
+        public void LogWhenInspectorsThrowExceptionAtCrawlCompleted()
+        {
+            // Arrange
+            var inspector = new Mock<IInspector>();
+            var exception = new Exception("Test exception");
+            inspector.Setup(i => i.CrawlCompleted()).Throws(exception);
+            var logHandler = new Mock<ILogHandler>();
+            var crawler = new Crawler(new Mock<ICrawl>().Object, logHandler.Object, new Mock<IExaminerFactory>().Object, new[] { inspector.Object });
+
+            // Act
+            crawler.Crawl(_uri);
+
+            // Assert
+            logHandler.Verify(x => x.Log(It.Is<InspectorLogMessage>(
+                l => l.Inspector.Name == inspector.Object.Name && 
+                l.LogMessage.LogType == LogType.Error && 
+                l.LogMessage.Exception == exception)), Times.Once);
         }
     }
 }
